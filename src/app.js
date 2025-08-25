745e74af16a4a72447e72499913f07255ab047c2
Pravin <pravinmishrag3@gmail.com>
2025-08-25 08:09:48 +0530

    removed unnecessory commented code and spaces

--- src/app.js
+++ src/app.js
@@ -3,13 +3,10 @@ const express = require("express");
 const { connectDB } = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
-// const jwt = require("jsonwebtoken");
-
 
 // middleware => it is reading and converting JSON request data into javaScript object
 app.use(express.json());
 app.use(cookieParser());
-
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
@@ -18,12 +15,6 @@ const requestRouter = require("./routes/request");
 app.use("/",authRouter);
 app.use("/",profileRouter);
 app.use("/",requestRouter);
-
-
-
-
-
-
 
 connectDB()
   .then(() => {
