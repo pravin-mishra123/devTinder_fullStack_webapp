754180eded1163d55fa384c6988db5faca73ff8c
Pravin Mishra <43815390+pravin-mishra123@users.noreply.github.com>
2025-10-28 09:14:20 +0530

    handle proper error message in logout api

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Token is not valid!!!!");
+      return res.status(401).send("Please Login");
     }
     const decodeObj = await jwt.verify(token, "Dev@Tinder$790");
     const { _id } = decodeObj;
